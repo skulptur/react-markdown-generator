02333d7cb3fc5b881297bd28bb419f1f169e7b0e
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-02 23:09:45 -0300

    fix index start

--- src/lib/ordered.ts
+++ src/lib/ordered.ts
@@ -2,4 +2,4 @@ import { prefix } from '../utils/prefix'
 import { lines } from '../utils/lines'
 
 export const ordered = (stringArray: Array<string>) =>
-  lines(stringArray.map((str, index) => prefix(`${index}. `, str)))
+  lines(stringArray.map((str, index) => prefix(`${index + 1}. `, str)))
